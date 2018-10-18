2a4a78b813d8c1e7b84c3763ed9330c598e0d15f
Tahar Meijs <tntmeijs@gmail.com>
2018-10-18 17:12:22 +0200

    Texture rendering
    Add texture to the triangle.

--- RayTracing/Resources/Shaders/simple_shader.hlsl
+++ RayTracing/Resources/Shaders/simple_shader.hlsl
@@ -1,20 +1,23 @@
+Texture2D g_texture : register(t0);
+SamplerState g_sampler : register(s0);
+
 struct VSOutput
 {
 	float4 position : SV_POSITION;
-	float4 color : COLOR;
+	float2 uv : TEXCOORD;
 };
 
-VSOutput vs_main(float4 position : POSITION, float4 color : COLOR)
+VSOutput vs_main(float4 position : POSITION, float2 uv : TEXCOORD)
 {
 	VSOutput result;
 
 	result.position = position;
-	result.color = color;
+	result.uv = uv;
 
 	return result;
 }
 
 float4 ps_main(VSOutput input) : SV_TARGET
 {
-	return input.color;
+	return g_texture.Sample(g_sampler, input.uv);
 }
