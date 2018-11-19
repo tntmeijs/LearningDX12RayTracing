f307bb6fe82de01979d57f6c3695dc466308480b
Tahar Meijs <tntmeijs@gmail.com>
2018-11-19 10:00:53 +0100

    Main renderer files
    [ADD] Empty renderer header file.
    [ADD] Empty renderer source file.

---
+++ RayTracing/Include/Renderer/Renderer.hpp
@@ -0,0 +1,19 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+namespace tnt
+{
+	namespace graphics
+	{
+		class Renderer
+		{
+		public:
+			Renderer();
+			~Renderer();
+
+		private:
+		};
+	}
+}
+
+#endif
---
+++ RayTracing/Source/Renderer/Renderer.cpp
@@ -0,0 +1,9 @@
+#include "Renderer/Renderer.hpp"
+
+tnt::graphics::Renderer::Renderer()
+{
+}
+
+tnt::graphics::Renderer::~Renderer()
+{
+}
